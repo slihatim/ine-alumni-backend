61e5f34c5c3ef83f03d748aa0e63ba41f7d5bc3a
hatim <hatim.slimani.19@gmail.com>
2025-05-02 03:13:10 +0100

    Changing quote

--- src/components/home/Section4.jsx
+++ src/components/home/Section4.jsx
@@ -9,13 +9,13 @@ const Section4 = () => {
                 <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z"/>
             </svg>
             <blockquote>
-                <p class="text-2xl italic font-medium text-gray-900 dark:text-white">"Success isn’t about how much money you make; it’s about the difference you make in people’s lives. And often, it’s the connections you build along the way that open those doors."</p>
+                <p class="text-2xl italic font-medium text-gray-900 dark:text-white">"Your network is your net worth."</p>
             </blockquote>
             <figcaption class="flex items-center justify-center mt-6 space-x-3 rtl:space-x-reverse">
-                <img class="w-6 h-6 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png" alt="profile picture" />
+                <img class="w-6 h-6 rounded-full" src="/assets/porter_gale.jpg" alt="profile picture" />
                 <div class="flex items-center divide-x-2 rtl:divide-x-reverse divide-gray-500 dark:divide-gray-700">
-                    <cite class="pe-3 font-medium text-gray-900 dark:text-white">Michelle Obama</cite>
-                    <cite class="ps-3 text-sm text-gray-500 dark:text-gray-400">Attorney</cite>
+                    <cite class="pe-3 font-medium text-gray-900 dark:text-white">Porter Gale</cite>
+                    <cite class="ps-3 text-sm text-gray-500 dark:text-gray-400">Auteure & Entrepreneure</cite>
                 </div>
             </figcaption>
         </figure>
