a9a8977bf983db142e9f36adbe1530245315363a
hatim <hatim.slimani.19@gmail.com>
2025-05-10 23:28:45 +0100

    Adding necessary dependencies + Structuring the project into packages

---
+++ src/main/java/com/ine/backend/IneAlumniBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ine.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IneAlumniBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IneAlumniBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ine/backend/IneAlumniBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ine.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IneAlumniBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
