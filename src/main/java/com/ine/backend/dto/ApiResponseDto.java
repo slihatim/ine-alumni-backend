88de257a78880e93ef8e936d632819ad27087a59
hatim <hatim.slimani.19@gmail.com>
2025-06-16 23:53:00 +0100

    Integrating ApiResponseDto into the API
    - Adding the Exception Handler for all exceptions.

--- src/main/java/com/ine/backend/dto/ApiResponseDto.java
+++ src/main/java/com/ine/backend/dto/ApiResponseDto.java
@@ -12,5 +12,5 @@ import lombok.NoArgsConstructor;
 public class ApiResponseDto<T> {
     private String message;
     private T response;
-    private Boolean success;
+    private Boolean isSuccess;
 }
--- src/main/java/com/ine/backend/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/ine/backend/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.ine.backend.exceptions;
 
+import com.ine.backend.dto.ApiResponseDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -10,10 +13,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+    public ResponseEntity<ApiResponseDto<String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
         return ResponseEntity
                 .badRequest()
-                .body(ex.getMessage());
+                .body(
+                        new ApiResponseDto(ex.getMessage(), null, false)
+                );
     }
 
     @ExceptionHandler(value = UserAlreadyExistsException.class)
@@ -21,5 +26,15 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.CONFLICT)
                 .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<ApiResponseDto<String>> handleException(Exception ex) {
+        Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+        logger.error(ex.getMessage());
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ApiResponseDto<>("Erreur, Veuillez ressayer.", null, false));
     }
 }
