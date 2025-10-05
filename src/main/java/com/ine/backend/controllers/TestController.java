6fd62173f6fbf1a5221a52dd934953cf296f2cde
Hatim SLIMANI <151152427+slihatim@users.noreply.github.com>
2025-10-05 21:55:29 +0100

    Make events public (#22)

--- src/main/java/com/ine/backend/controllers/TestController.java
+++ src/main/java/com/ine/backend/controllers/TestController.java
@@ -1,12 +1,10 @@
 package com.ine.backend.controllers;
 
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/v1/test")
 public class TestController {
