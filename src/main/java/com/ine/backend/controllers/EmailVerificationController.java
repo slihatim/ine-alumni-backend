5de926d7f2cfc5d09588d2e083181caecf0ff422
hatim <hatim.slimani.19@gmail.com>
2025-10-31 14:36:56 +0100

    formatting

--- src/main/java/com/ine/backend/controllers/EmailVerificationController.java
+++ src/main/java/com/ine/backend/controllers/EmailVerificationController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.ine.backend.dto.ApiResponseDto;
-import com.ine.backend.entities.User;
 import com.ine.backend.exceptions.UserNotFoundException;
 import com.ine.backend.services.EmailVerificationService;
 
